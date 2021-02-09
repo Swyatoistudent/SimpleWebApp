5a29549d81a12d0520413f9842648202d8f31934
user <swyat.usatov@gmail.com>
2021-02-09 17:32:03 +0200

    ADD

--- database.py
+++ database.py
@@ -2,37 +2,39 @@ import sqlite3
 from flask import g, Flask
 import click
 from flask.cli import with_appcontext
+import sqlalchemy
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+engine = sqlalchemy.create_engine('sqlite:///my.db')
 
 
-def get_db():
-    db = getattr(g, "_database", None)
-    if db is None:
-        db = g._database = sqlite3.connect("mydb.db")
-    db.row_factory = sqlite3.Row
-    return db
+class Todolists(Base):
+    __tablename__ = 'todolists'
+    id = sqlalchemy.Column(Integer, primary_key=True, autoincrement=True)
+    project_name = sqlalchemy.Column(String)
 
 
-def init_db():
-    db = get_db()
-    cursor = db.cursor()
-    cursor.executescript("""CREATE TABLE IF NOT EXISTS todolists(
-                    id INTEGER  PRIMARY KEY AUTOINCREMENT,
-                    project_name TEXT
-                    );
-    """)
-    cursor.executescript("""CREATE TABLE IF NOT EXISTS project(
-          item_id INTEGER PRIMARY KEY AUTOINCREMENT,
-          project_id INTEGER,
-          item TEXT,
-          foreign key (project_id) references todolists (id)
-    );
-    """)
+class project(Base):
+    __tablename__ = 'project'
+    item_d = sqlalchemy.Column(Integer, primary_key=True, autoincrement=True)
+    project_id = sqlalchemy.Column(Integer, ForeignKey('todolists.id'))
+    item = sqlalchemy.Column(String)
+
+    # cursor = db.cursor()
+    # cursor.executescript("""CREATE TABLE IF NOT EXISTS todolists(
+    #                 id INTEGER  PRIMARY KEY AUTOINCREMENT,
+    #                 project_name TEXT
+    #                 );
+    # """)
+    # cursor.executescript("""CREATE TABLE IF NOT EXISTS project(
+    #       item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #       project_id INTEGER,
+    #       item TEXT,
+    #       foreign key (project_id) references todolists (id)
+    # );
+    # """)
 
 
-
-
-@click.command('init-db')
-@with_appcontext
-def db_init():
-    init_db()
-
+Base.metadata.create_all(engine)
--- hello.py
+++ hello.py
@@ -1,59 +1,85 @@
+import click
 from flask import Flask
 import flask
-import database
+from sqlalchemy.orm import sessionmaker
+
+import sqlalchemy
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+engine = sqlalchemy.create_engine('sqlite:///my.db')
+
+
+class Todolists(Base):
+    __tablename__ = 'todolists'
+    id = sqlalchemy.Column(Integer, primary_key=True, autoincrement=True)
+    project_name = sqlalchemy.Column(String)
+    def __init__(self,project_name):
+        self.project_name = project_name
+
+class Project(Base):
+    __tablename__ = 'project'
+    item_d = sqlalchemy.Column(Integer, primary_key=True, autoincrement=True)
+    project_id = sqlalchemy.Column(Integer, ForeignKey('todolists.id'))
+    item = sqlalchemy.Column(String)
+
+    def __init__(self, project_id,item):
+        self.project_id = project_id
+        self.item = item
 
 app = Flask(__name__)
-database.init_db()
+Session = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
+
+
 @app.route('/')
 def root():
-    db = database.get_db()
-    cursor = db.cursor()
-    cursor.execute("""SELECT * FROM todolists""")
-    todolists = cursor.fetchall()
+    session = Session()
+    todolists =session.query(Todolists).all()
+    session.close()
     return flask.render_template("root.html", data=todolists)
 
 
 @app.route('/project_add', methods=["POST"])
 def put_project():
-    db = database.get_db()
-    cursor = db.cursor()
-    cursor.execute("""INSERT INTO todolists(project_name) VALUES ('{}')""".format(flask.request.form['project']))
-    db.commit()
+    session = Session()
+    session.add(Todolists(flask.request.form['project']))
+    session.commit()
+    session.close()
     return flask.redirect(flask.url_for('root'))
 
 
 @app.route('/delete_project', methods=["POST"])
 def delete_project():
-    db = database.get_db()
-    cursor = db.cursor()
-    cursor.execute(f"""DELETE FROM project where project_id = {flask.request.form['delete_project']}""")
-    cursor.execute(f"""DELETE FROM todolists where id = {flask.request.form['delete_project']}""")
-    db.commit()
+    session = Session()
+    session.query(Todolists).filter_by(id = flask.request.form['delete_project']).delete()
+    session.query(Project).filter_by(project_id = flask.request.form['delete_project']).delete()
+    session.commit()
+    session.close()
     return flask.redirect(flask.url_for('root'))
 
 
-@app.route('/<id>', methods=["GET"])
+@app.route('/<id>')
 def todolist(id):
-    db = database.get_db()
-    cursor = db.cursor()
-    cursor.execute(f"""SELECT * FROM project Where project_id = {id}""")
-    items_list = cursor.fetchall();
+    session = Session()
+    items_list = session.query(Project).filter_by(item_d = id)
     return flask.render_template("index.html", data=items_list, project_id=id)
 
 
 @app.route('/<id>/item_add', methods=["POST"])
 def put_item(id):
-    db = database.get_db()
-    cursor = db.cursor()
-    cursor.execute(f"""INSERT INTO project(project_id,item) VALUES ('{id}','{flask.request.form['item']}')""")
-    db.commit()
+    session = Session()
+    session.add(Project(flask.request.form['item'],id))
+    session.commit()
+    session.close()
     return flask.redirect(f'/{id}')
 
 
 @app.route('/<id>/delete_item', methods=["POST"])
 def delete_item(id):
-    db = database.get_db()
-    cursor = db.cursor()
-    cursor.execute(f"""DELETE FROM project WHERE item_id = {flask.request.form['delete_files']}""")
-    db.commit()
+    session = Session()
+    session.query(Project).filter_by(item_d = flask.request.form['delete_files']).delete()
+    session.commit()
+    session.close()
     return flask.redirect(f'/{id}')
